ff10d7e5a4cc5a2d9a997649cb41e35a2c0cd055
Martine Tan <martine.tan@outlook.com>
2022-09-27 12:05:17 +0200

    Made the networks convolutional

--- utils.py
+++ utils.py
@@ -9,10 +9,9 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 def plot_reconstructed(model, ax0=(-5, 5), ax1=(-5,5), n=12, dims=[0,1], img_size = (28,28), figsize=(8,8)):
     """ Sample uniformly from the latent space, and see how the decoder reconstructs inputs from arbitrary latents. """
 
-    #z = [0]*model.dim_latent
     for i, y in enumerate(np.linspace(*ax0, n)):
         for j, x in enumerate(np.linspace(*ax1, n)):
-            z = [0]*model.dim_latent
+            z = [0]*model.latent_dim
             z[dims[0]] = x
             z[dims[1]] = y
             z = torch.Tensor([z]).to(DEVICE)
@@ -34,7 +33,7 @@ def plot_latent_space2d(model, data, dims=[0,1], n_batches=100, device=DEVICE):
     with torch.no_grad():
         for i, (x, labels) in enumerate(data):
             batch_size = x.size(0)
-            x = x.view(batch_size, x.size(-2)*x.size(-1))
+            #x = x.view(batch_size, x.size(-2)*x.size(-1))   # Flattening the image
             z = model.get_latents(x.to(DEVICE))
             z = z.cpu().detach().numpy()
             plt.scatter(z[:, dims[0]], z[:, dims[1]], c = labels, cmap = 'tab20')
@@ -50,7 +49,7 @@ def plot_latent_space_tsne(model, data, n_batches=100, device=DEVICE):
     with torch.no_grad():
         for i, (x, l) in enumerate(data):
             batch_size = x.size(0)
-            x = x.view(batch_size, x.size(-2)*x.size(-1))
+            #x = x.view(batch_size, x.size(-2)*x.size(-1))  # Flattening the image
             z = model.get_latents(x.to(DEVICE))
             z = z.cpu().detach().numpy()
 
@@ -76,7 +75,7 @@ def plot_latent_space_pca(model, data, n_batches=100, device=DEVICE):
     with torch.no_grad():
         for i, (x, l) in enumerate(data):
             batch_size = x.size(0)
-            x = x.view(batch_size, x.size(-2)*x.size(-1))
+            #x = x.view(batch_size, x.size(-2)*x.size(-1)) # Flattening the image
             z = model.get_latents(x.to(DEVICE))
             z = z.cpu().detach().numpy()
 
